007266080e2963772fd6b69f2ce5c56d806e452f
Guthrie Ducheneaux <guthdx@gmail.com>
2025-05-15 11:04:43 -0600

    Initial push of website files

---
+++ rename_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Navigate to the folder containing the files
+# cd /path/to/test.mbiri.net/webpage
+
+# Rename files
+mv "Antibody Response After COVID-19 Vaccination" "antibody-response-after-covid-19-vaccination.html"
+mv "Pre-pandemic Spirometry and COVID-19 Outcomes" "pre-pandemic-spirometry-and-covid-19-outcomes.html"
+mv "Cardiovascular Health and COVID-19 Severity" "cardiovascular-health-and-covid-19-severity.html"
+mv "Neighborhood Data in the C4R Study" "neighborhood-data-in-the-c4r-study.html"
+mv "COVID-19 Recovery in the C4R Study" "covid-19-recovery-in-the-c4r-study.html"
+mv "COVID-19 Events Classification in the C4R Study" "covid-19-events-classification-in-the-c4r-study.html"
+mv "C4R Study Design" "c4r-study-design.html"
+mv "Resilience During the COVID-19 Pandemic" "resilience-during-the-covid-19-pandemic.html"
